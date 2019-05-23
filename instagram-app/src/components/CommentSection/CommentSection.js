48f49c2ffd677ce43d2b5aba41ba4530f763fbc8
Ihab Ali <ihab@live.com>
2019-05-23 07:36:12 -0500

    Add comments

--- instagram-app/src/components/CommentSection/CommentSection.js
+++ instagram-app/src/components/CommentSection/CommentSection.js
@@ -4,38 +4,38 @@ import PostInput from '../PostInput';
 import Comment from '../SingleComment.js';
 
 class CommentSection extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      comments: props.comments,
-      msg: '',
-      comment: ''
+    constructor(props){
+      super(props);
+      this.state = {
+        comments: props.comments,
+        msg: '',
+        comment: ''
+      }
     }
+  
+  changeHandler = e => {
+    this.setState({msg: e.target.value});
   }
-
-changeHandler = e => {
-  this.setState({msg: e.target.value});
-}
-
-post = () => {
-  this.setState({comment: `${this.state.msg}`})
-}
-
-// type="text" value={props.comment} change={props.changeHandler} placeholder="Add comment"
-
-  render(){
-  return (
-    <div>
-      {this.state.comments.map((c, i) => <Comment key={i} comment={c}/>)}
-      <p>{this.state.comment}</p>
-      <PostInput
-      comment={this.state.msg}
-      changeHandler={this.changeHandler}
-      change={this.post}
-      />
-    </div>
-  );
-}
-}
-
-export default CommentSection;
+  
+  post = () => {
+    this.setState({comment: `${this.state.msg}`})
+  }
+  
+  // type="text" value={props.comment} change={props.changeHandler} placeholder="Add comment"
+  
+    render(){
+    return (
+      <div>
+        {this.state.comments.map((item) => <Comment comment={item}/>)}
+        <p>{this.state.comment}</p>
+        <PostInput
+        comment={this.state.msg}
+        changeHandler={this.changeHandler}
+        change={this.post}
+        />
+      </div>
+    );
+  }
+  }
+  
+  export default CommentSection;  
--- instagram-app/src/components/PostHeader.js
+++ instagram-app/src/components/PostHeader.js
@@ -2,17 +2,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+//Here I am creating a component that comprises just the thumbnail and the username
+//because it will be easier to style them as a single component. This component
+//is passed into the Post component where it is combined with the imageUrl,
+//the CommentSection component, and the likes component
 const PostHeader = props => {
   return (
     <div>
-      <div className="profilePic">
+      <div className="profile">
         <img
-          alt="profile picture"
-          src={props.thumbnailUrl}
-          />
+        alt="user profile picture"
+        src={props.thumbnailUrl}
+        />
       </div>
       <div className="userName">
-        <strong>{props.username}</strong>
+        <h4>{props.username}</h4>
       </div>
     </div>
 )
